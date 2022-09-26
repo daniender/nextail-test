3dd277d376d52f710a1fd8a1f2af217b871ffd36
Daniel Alvarez <daniender@gmail.com>
2022-09-26 17:26:52 +0200

    added graphic library

--- stock-app/src/app/app.module.ts
+++ stock-app/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { SharedModule } from './modules/shared.module';
 import { MaterialModule } from './modules/material.module';
 import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 import { environment } from 'src/environments/environment';
+import { NgChartsModule } from 'ng2-charts';
 
 const socketConfig: SocketIoConfig = { url: environment.socketUrl, options: {}};
 
@@ -22,6 +23,7 @@ const socketConfig: SocketIoConfig = { url: environment.socketUrl, options: {}};
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    NgChartsModule,
     ComponentsModule,
     HttpClientModule,
     LayoutsModule,
